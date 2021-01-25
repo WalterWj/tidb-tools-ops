7ab20d0e0a906ccb3564aef057e3310fabd6ab00
walterwj <277372664@qq.com>
2021-01-25 16:54:10 +0800

    master

--- cmd/export.go
+++ cmd/export.go
@@ -17,9 +17,8 @@ package cmd
 import (
 	"database/sql"
 	"fmt"
+	"os"
 	"strings"
-
-	_ "github.com/Walterwj/tidb-tools-ops/common"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/spf13/cobra"
@@ -60,8 +59,8 @@ var exportCmd = &cobra.Command{
 
 			// fmt.Println(createuser)
 			// fmt.Println(userinfo)
-			Addfile(createuser)
-			Addfile(userinfo)
+			addfile(createuser)
+			addfile(userinfo)
 			gRows, err := db.Query(grantQ)
 			if err != nil {
 				fmt.Printf("execute %v fail", grantQ)
@@ -73,9 +72,9 @@ var exportCmd = &cobra.Command{
 				}
 				grant = strings.Join([]string{grant, ";"}, "")
 				// fmt.Println(grant)
-				Addfile(grant)
+				addfile(grant)
 			}
-			Addfile("")
+			addfile("")
 		}
 
 		fmt.Println("Successfully introduce all users and permissions.")
@@ -93,10 +92,10 @@ func init() {
 
 }
 
-// func addfile(context string) {
-// 	f, _ := os.OpenFile("users.sql", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
-// 	context = strings.Join([]string{context, "\n"}, "")
-// 	f.WriteString(context)
-// 	defer f.Close()
-// 	// fmt.Printf("Write %v sucessfully \n", context)
-// }
+func addfile(context string) {
+	f, _ := os.OpenFile("users.sql", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	context = strings.Join([]string{context, "\n"}, "")
+	f.WriteString(context)
+	defer f.Close()
+	// fmt.Printf("Write %v sucessfully \n", context)
+}
