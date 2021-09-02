94561bb3c3b6bd7906e2c8338b6e88249f3ed6a9
walterwj <277372664@qq.com>
2021-09-02 18:45:09 +0800

    add statsdump

---
+++ cmd/statsdump.go
@@ -0,0 +1,65 @@
+// Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	// import common
+	_ "github.com/WalterWj/tidb-tools-ops/common"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	dbhost, dbname, dbusername, dbpassword string
+	dbport                                 int
+)
+
+const (
+	tablesQ = "show tables"
+)
+
+// statsdumpCmd represents the statsdump command
+var statsdumpCmd = &cobra.Command{
+	Use:   "statsdump",
+	Short: "Export statistics and table structures",
+	Long:  `Export statistics and table structures`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("statsdump called")
+		c := schemainfo()
+		print(c)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(statsdumpCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// statsdumpCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// statsdumpCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	statsdumpCmd.Flags().StringVarP(&dbusername, "dbusername", "u", "root", "Database user")
+	statsdumpCmd.Flags().StringVarP(&dbname, "dbname", "d", "test", "Database name")
+	statsdumpCmd.Flags().StringVarP(&dbhost, "dbhost", "H", "127.0.0.1", "Database host")
+	statsdumpCmd.Flags().StringVarP(&dbpassword, "dbpassword", "p", "123456", "Database passowrd")
+	statsdumpCmd.Flags().IntVarP(&dbport, "dbport", "P", 4000, "Database Port")
+	statsdumpCmd.Flags().IntVarP(&dbport, "statusport", "s", 10080, "TiDB Status Port")
+}
---
+++ common/schemainfo.go
@@ -0,0 +1,32 @@
+package common
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func init() {
+	fmt.Println("get schema information mould init funcation")
+}
+
+// Get table schema information
+func getTables(db *sql.DB, dbname string) map[int]string {
+	var r = make(map[int]string)
+	tablesQ := fmt.Sprintf("select table_name from information_schema.tables where TABLE_SCHEMA in (%s);", dbname)
+	rows, err := db.Query(tablesQ)
+	if err != nil {
+		fmt.Printf("execute %v fail", tablesQ)
+	}
+	defer rows.Close()
+	n := 0
+	for rows.Next() {
+		var t string
+		err := rows.Scan(&t)
+		if err != nil {
+			fmt.Printf("rows scan fail")
+		}
+		r[n] = t
+		n++
+	}
+	return r
+}
