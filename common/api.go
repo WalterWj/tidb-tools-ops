ee742dcdfef6041dd2651f2cabe64cb6c0912378
walterwj <277372664@qq.com>
2021-09-15 12:51:08 +0800

    master

--- common/api.go
+++ common/api.go
@@ -1,6 +1,20 @@
 package common
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func init() {
 	// api.go init
 }
 
+// parser table stats
+func ParserTs(StatusPort int, DbIp string, dbName string, tbName string) {
+	req, err := http.Get(fmt.Sprintf("http://%s:%v/stats/dump/%s/%s", DbIp, StatusPort, dbName, tbName))
+	if err != nil {
+		//
+	}
+	defer req.Body.Close()
+	fmt.Println(req)
+}
