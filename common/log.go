5308932abfa4667a2afd7608d592dd44ecdbc0e6
walterwj <277372664@qq.com>
2021-09-18 20:08:50 +0800

    master

--- common/log.go
+++ common/log.go
@@ -1,10 +1,20 @@
 package common
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"time"
+)
 
 // error log
 func IfErrLog(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+// error print
+func IfErrPrint(err string) {
+	ct := time.Now().Format("2006-01-02 15:04:05")
+	fmt.Printf("%s ERROR: %s", ct, err)
 }
