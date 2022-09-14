068e6a448cccb85dbb22d632456034af2146b3b8
WalterWj <277372664@qq.com>
2022-09-14 14:58:49 +0800

    main

--- pkg/logutil/ttoLog.go
+++ pkg/logutil/ttoLog.go
@@ -1,7 +1,53 @@
 package logutil
 
-import "log"
+import (
+	"log"
+	"os"
+)
 
-func InfoLog() {
-	log.Println("Content from log package!")
+// interface
+type Logs interface {
+	// init log file
+	InitLog()
+	// write info log file
+	InfoLog()
 }
+
+var (
+	WarningLogger *log.Logger
+	InfoLogger    *log.Logger
+	ErrorLogger   *log.Logger
+)
+
+type Content struct {
+	Content string
+	Mode    string `default:"info"`
+}
+
+func InitLog(fileName string) {
+	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	InfoLogger = log.New(file, "[INFO]: ", log.Ldate|log.Ltime|log.Lshortfile)
+	WarningLogger = log.New(file, "[WARNING]: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger = log.New(file, "[ERROR]: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func InfoLog(c string) {
+	InfoLogger.Println(c)
+}
+
+func ErrorLog(c string) {
+	ErrorLogger.Println(c)
+}
+
+func WarningLog(c string) {
+	WarningLogger.Println(c)
+}
+
+// use
+// func InfoLog() {
+// 	InfoLogger.Println("Starting the application...")
+// }
--- pkg/logutil/ttoLog_test.go
+++ pkg/logutil/ttoLog_test.go
@@ -1,1 +1,8 @@
-package logutil_test
+package logutil
+
+import "testing"
+
+func TestInfoLog(t *testing.T) {
+	want := "Content from log package!"
+	logMsg := InfoLog()
+}
