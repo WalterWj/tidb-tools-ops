a899fddde6bb5d1c5d6137da20460e731fc8f233
WalterWj <277372664@qq.com>
2022-09-14 16:35:51 +0800

    add logs util

--- pkg/logutil/ttoLog.go
+++ pkg/logutil/ttoLog.go
@@ -1,6 +1,7 @@
 package logutil
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
@@ -11,43 +12,50 @@ type Logs interface {
 	InitLog()
 	// write info log file
 	InfoLog()
+	// write error log file
+	ErrorLog()
+	// write warning log file
+	WarningLog()
 }
 
+// var warning/info/error log
 var (
 	WarningLogger *log.Logger
 	InfoLogger    *log.Logger
 	ErrorLogger   *log.Logger
 )
 
+// log content
 type Content struct {
 	Content string
-	Mode    string `default:"info"`
 }
 
-func InitLog(fileName string) {
+// init log file
+func InitLog(fileName string) error {
 	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		return err
 	}
 
-	InfoLogger = log.New(file, "[INFO]: ", log.Ldate|log.Ltime|log.Lshortfile)
-	WarningLogger = log.New(file, "[WARNING]: ", log.Ldate|log.Ltime|log.Lshortfile)
-	ErrorLogger = log.New(file, "[ERROR]: ", log.Ldate|log.Ltime|log.Lshortfile)
+	InfoLogger = log.New(file, "[INFO] ", log.Ldate|log.Ltime)
+	WarningLogger = log.New(file, "[WARNING] ", log.Ldate|log.Ltime)
+	ErrorLogger = log.New(file, "[ERROR] ", log.Ldate|log.Ltime)
+
+	return nil
 }
 
-func InfoLog(c string) {
-	InfoLogger.Println(c)
+// info log
+func (c Content) InfoLog() {
+	InfoLogger.Println(c.Content)
 }
 
-func ErrorLog(c string) {
-	ErrorLogger.Println(c)
+// error log
+func (c Content) ErrorLog() {
+	ErrorLogger.Println(c.Content)
 }
 
-func WarningLog(c string) {
-	WarningLogger.Println(c)
+// warning log
+func (c Content) WarningLog() {
+	WarningLogger.Println(c.Content)
 }
-
-// use
-// func InfoLog() {
-// 	InfoLogger.Println("Starting the application...")
-// }
--- pkg/logutil/ttoLog_test.go
+++ pkg/logutil/ttoLog_test.go
@@ -1,8 +1,34 @@
 package logutil
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestInitlog(t *testing.T) {
+	err := InitLog("/root/test.test")
+	want := "open /root/test.test: permission denied"
+	if want != err.Error() {
+		t.Fatalf(`InitLog("/root/test.test") = %s, want "", error`, err.Error())
+	}
+}
 
 func TestInfoLog(t *testing.T) {
-	want := "Content from log package!"
-	logMsg := InfoLog()
+	var log Content
+	InitLog("test.test")
+	log = Content{"info msg"}
+	log.InfoLog()
+	log = Content{"error msg"}
+	log.ErrorLog()
+	log = Content{"warning msg"}
+	log.WarningLog()
+	data, _ := os.ReadFile("test.test")
+	want := 116
+	// delete tmp file
+	defer os.Remove("test.test")
+	fmt.Println(len(data))
+	if want != len(data) {
+		t.Fatalf(`%d`, len(data))
+	}
 }
