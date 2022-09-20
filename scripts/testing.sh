4f71f2d2449d82ec91b8603a354daab8dbdb6a43
WalterWj <277372664@qq.com>
2022-09-20 18:05:14 +0800

    add testing.sh

---
+++ scripts/testing.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+dir=$1
+cd "${dir}" || exit 1
+
+# 生产 out 文件，run testing
+go test -v -covermode=set -coverprofile=$hint_test.out ./
+
+# 查看测试覆盖率
+go tool cover -func=hint_test.out
+
+# 生产测试覆盖率 HTML
+go tool cover -html=hint_test.out -o hint_test.html
